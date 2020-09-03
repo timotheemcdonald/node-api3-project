db40ef49b70aeccdd0865ccf6010b2355b632030
timotheemcdonald <timotheemcdonald@gmail.com>
2020-09-03 14:08:35 -0400

    adding env and dynamic port

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // code away!
-
+require('dotenv').config()
 const server = require('./server')
 
-server.listen(7000, () => {
-    console.log( '\n* Server running on http://localhost:7000  *\n' )
+const port = process.env.PORT || 7000;
+
+server.listen(port, () => {
+    console.log( `\n* Server running on http://localhost:${port} *\n` )
 })
